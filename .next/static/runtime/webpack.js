a2c2b5ee741739f52fa6678ebe6f428a1a38adaf
Halil Dogan <olush.dogan2gmail.com>
2021-04-18 13:57:33 -0400

    l

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "32ad284e96802f558e80";
+/******/ 	var hotCurrentHash = "f3dd661bac98e111348f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- server.js
+++ server.js
@@ -1,21 +1,45 @@
-const express = require('express')
-const next = require('next')
-const nextI18NextMiddleware = require('next-i18next/middleware').default
+const express = require('express');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const next = require('next');
+const nextI18NextMiddleware = require('next-i18next/middleware').default;
 
-const nextI18next = require('./i18n')
-
-const port = process.env.PORT || 7006
-const app = next({ dev: process.env.NODE_ENV !== 'production' })
+const nextI18next = require('./i18n');
+const ports = {
+  http: process.env.PORT || 7006,
+  https: process.env.PORT_SSL || 8006
+};
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
-(async () => {
-  await app.prepare()
-  const server = express()
+app.prepare().then(() => {
+  const server = express();
+  server.use(nextI18NextMiddleware(nextI18next));
 
-  server.use(nextI18NextMiddleware(nextI18next))
+  server.use(bodyParser.json());
+  server.use(bodyParser.urlencoded({ extended: true }));
 
-  server.get('*', (req, res) => handle(req, res))
+  server.use(cors());
 
-  await server.listen(port)
-  console.log(`> Ready on ${process.env.HOST}`) // eslint-disable-line no-console
-})()
+  server.all('*', (req, res) => handle(req, res));
+  http.createServer(server).listen(ports.http, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${ports.http}`);
+  });
+  if (process.env.NODE_ENV === 'production') {
+    const options = {
+      key: fs.readFileSync('/home/admin/conf/web/ssl.navi.archi.key'),
+      cert: fs.readFileSync('/home/admin/conf/web/ssl.navi.archi.crt'),
+    };
+    https.createServer(options, server).listen(ports.https, (err) => {
+      if (err) throw err;
+      console.log(`> Ready on http://localhost:${ports.https}`);
+    });
+  }
+}).catch((ex) => {
+  console.error(ex.stack);
+});
